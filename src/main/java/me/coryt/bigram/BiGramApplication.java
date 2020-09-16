c7582ca8c8c7d1014050a51e00d32ee1b5c5e89f
Cory Krol <16892390+corykrol@users.noreply.github.com>
2020-09-15 22:14:42 -0500

    Add spring boot to project because I'm lazy. Add some helper methods

---
+++ src/main/java/me/coryt/bigram/BiGramApplication.java
@@ -0,0 +1,12 @@
+package me.coryt.bigram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiGramApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BiGramApplication.class);
+	}
+}
