879d2dfe3c0fbe43edaae8271cf8c64eeec4e804
Cory Krol <16892390+corykrol@users.noreply.github.com>
2020-09-15 22:30:34 -0500

    Add logging properties

--- src/main/java/me/coryt/bigram/BiGramApplication.java
+++ src/main/java/me/coryt/bigram/BiGramApplication.java
@@ -3,7 +3,7 @@ package me.coryt.bigram;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"me.coryt.bigram"})
 public class BiGramApplication {
 	
 	public static void main(String[] args) {
