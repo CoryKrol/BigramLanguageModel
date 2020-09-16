e6cd491c99f202e2c1023cf8ef18802d81ad6c4f
Cory Krol <16892390+corykrol@users.noreply.github.com>
2020-09-16 01:18:53 -0500

    Finish corpus tokenization code and unit tests

--- src/main/java/me/coryt/bigram/util/TextProcessingUtil.java
+++ src/main/java/me/coryt/bigram/util/TextProcessingUtil.java
@@ -9,14 +9,36 @@ import java.util.List;
 @UtilityClass
 public class TextProcessingUtil {
 	
+	/**
+	 * Split corpus on new lines
+	 *
+	 * @param corpus string of text
+	 * @return list of sentences
+	 */
 	public List<String> splitCorpus(String corpus) {
 		return Arrays.asList(corpus.split("\\n"));
 	}
 	
+	/**
+	 * Split a list of sentences into a list of sentence tokens
+	 *
+	 * @param corpus list of sentences
+	 * @return list of sentences represented as tokens
+	 */
 	public List<List<String>> splitSentences(List<String> corpus) {
 		List<List<String>> sentenceList = new ArrayList<>();
 		corpus.forEach(str -> sentenceList.add(Arrays.asList(str.split("\\s"))));
 		return sentenceList;
+	}
+	
+	/**
+	 * Replace all non-letter/non-whitespace characters and remove any spurious whitespace around them
+	 *
+	 * @param corpus input text corpus string
+	 * @return processed corpus string
+	 */
+	public String removeNonWordCharacters(String corpus) {
+		return corpus.replaceAll("\\h*([^a-zA-z\\s]|\\[|])+\\h*", "").trim();
 	}
 	
 }
--- src/test/java/me/coryt/bigram/util/TextProcessingUtilTest.java
+++ src/test/java/me/coryt/bigram/util/TextProcessingUtilTest.java
@@ -1,13 +1,12 @@
 package me.coryt.bigram.util;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static me.coryt.bigram.util.TextProcessingUtil.splitCorpus;
-import static me.coryt.bigram.util.TextProcessingUtil.splitSentences;
+import static me.coryt.bigram.util.TextProcessingUtil.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TextProcessingUtilTest {
 	private static final String TEST_CORPUS = "[ moby dick by herman melville 1851 ]\n" +
@@ -19,23 +18,40 @@ class TextProcessingUtilTest {
 	private static final int TEST_SENTENCE_TWO_NUMBER_OF_TOKENS = 2;
 	private static final int TEST_SENTENCE_THREE_NUMBER_OF_TOKENS = 12;
 	
+	private static final int TEST_SENTENCE_CLEAN_NUM_TOKENS = 16;
+	
 	
 	@DisplayName("Should split test corpus into ArrayList of size 3")
 	@Test
 	void testSplitCorpus() {
 		List<String> result = splitCorpus(TEST_CORPUS);
-		Assertions.assertEquals(TEST_CORPUS_NUM_SENTENCES, result.size());
+		assertEquals(
+				TEST_CORPUS_NUM_SENTENCES,
+				result.size());
 	}
 	
 	@DisplayName("Should split test corpus into ArrayList of size 3 containing token array lists")
 	@Test
 	void testTokenizeWords() {
 		List<List<String>> result = splitSentences(splitCorpus(TEST_CORPUS));
+		assertEquals(
+				TEST_SENTENCE_ONE_NUMBER_OF_TOKENS,
+				result.get(0).size());
+		assertEquals(
+				TEST_SENTENCE_TWO_NUMBER_OF_TOKENS,
+				result.get(1).size());
+		assertEquals(
+				TEST_SENTENCE_THREE_NUMBER_OF_TOKENS,
+				result.get(2).size());
 		
-		Assertions.assertEquals(TEST_CORPUS_NUM_SENTENCES, result.size());
-		Assertions.assertEquals(TEST_SENTENCE_ONE_NUMBER_OF_TOKENS, result.get(0).size());
-		Assertions.assertEquals(TEST_SENTENCE_TWO_NUMBER_OF_TOKENS, result.get(1).size());
-		Assertions.assertEquals(TEST_SENTENCE_THREE_NUMBER_OF_TOKENS, result.get(2).size());
-		
+	}
+	
+	@DisplayName("Should remove non-character & non-white space leaving 16 tokens")
+	@Test
+	void testRemoveNonWordCharacters() {
+		String result = removeNonWordCharacters(TEST_CORPUS);
+		assertEquals(
+				TEST_SENTENCE_CLEAN_NUM_TOKENS,
+				result.split("\\s").length);
 	}
 }
